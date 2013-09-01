148e09d438aff8b5ff7b1dd627ad0db8ba1266d4
Panchami S <panchamisanjeev@gmail.com>
2013-09-01 10:45:49 +0530

    Programs of lab2, displaying employee details without using @property and @syntesize

---
+++ emp.m
@@ -0,0 +1,21 @@
+#import "emp.h"
+//implementation of the functions declared in "emp.h"
+@implementation Employee
+
+-(void)SetName:(char*)nm
+{
+	strcpy(name,nm);
+}
+-(void)SetDept:(char*)dt
+{
+	strcpy(dept,dt);
+}
+-(void)SetEid:(char*)ID
+{
+	strcpy(eid,ID);
+}
+-(void)print
+{
+	printf("The employee details are \n Name: %s \n Dept:%s \n ID: %s\n",name,dept,eid);
+}
+@end
